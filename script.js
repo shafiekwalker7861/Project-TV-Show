1150f8e0de30a358046bb9a471b7f9b5a4530968
shafiekwalker7861 <westerncapetowndesigner@gmail.com>
2026-08-09 11:54:20 +0200

    Complete TV Show Project level 100

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//You can edit ALL of the code here
+// You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -6,7 +7,36 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const episodeCount = document.getElementById("episode-count");
+
+  episodeCount.textContent = `Got ${episodeList.length} episode(s)`;
+
+  for (const episode of episodeList) {
+    const episodeName = episode.name;
+
+    const seasonNumber = String(episode.season).padStart(2, "0");
+    const episodeNumber = String(episode.number).padStart(2, "0");
+    const episodeCode = `S${seasonNumber}E${episodeNumber}`;
+
+    const episodeCard = document.createElement("div");
+    episodeCard.classList.add("episode-card");
+
+    const heading = document.createElement("h2");
+    heading.textContent = `${episodeName} - ${episodeCode}`;
+
+    const episodeImage = document.createElement("img");
+    episodeImage.src = episode.image.medium;
+    episodeImage.alt = `Image for ${episodeName}`;
+
+    const episodeSummary = document.createElement("div");
+    episodeSummary.innerHTML = episode.summary;
+
+    episodeCard.appendChild(heading);
+    episodeCard.appendChild(episodeImage);
+    episodeCard.appendChild(episodeSummary);
+
+    rootElem.appendChild(episodeCard);
+  }
 }
 
 window.onload = setup;
